8303ddb50f4c157c9af7fe652a881a9cc8d51af1
Rogério Carvalho <rogerio.carvalho@unifesspa.edu.br>
2021-02-24 11:23:04 -0300

    Lista de contatos - versão 0.1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, {useEffect, useRef, useState} from "react";
 import './App.css';
+import Tabletop from 'tabletop'
+import {Card, CardColumns, Form} from "react-bootstrap";
+import {FaWhatsapp} from "react-icons/all";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([]);
+    const [filtered, setFiltered] = useState([]);
+    const inputRef = useRef(null);
+
+    const onInputChanged = () => {
+        console.log(process.env.REACT_APP_SPREEDSHEET_ID)
+        setFiltered(data.filter(item => (
+            item.Base.toLowerCase().includes(inputRef.current.value.toLowerCase()) ||
+            item.Executivo.toLowerCase().includes(inputRef.current.value.toLowerCase())
+        )));
+    }
+
+
+    useEffect(() => {
+        Tabletop.init({
+            key: process.env.REACT_APP_SPREEDSHEET_ID,
+            simpleSheet: true
+        })
+            .then((data) => {
+                setData(data)
+                setFiltered(data)
+            })
+            .catch((err) => console.warn(err));
+    }, []);
+
+    return (
+        <>
+            <Form>
+                <Form.Group>
+                    <Form.Control ref={inputRef} size="lg" type="text" placeholder="Pesquise uma Base ou um Executivo"
+                                  onChange={onInputChanged}/>
+                </Form.Group>
+            </Form>
+
+            <CardColumns>
+                {filtered.map((item, i) => (
+
+                    <Card key={i} bg="light" text="dark" border="primary">
+                        <Card.Body>
+                            <Card.Title>
+                                Base: {item.Base}
+                            </Card.Title>
+                            <Card.Text>
+                                {item.Executivo}
+                            </Card.Text>
+                            <Card.Footer><a href={"https://api.whatsapp.com/send?phone=55" + item.Telefone
+                            + encodeURI("Olá, " + item.Executivo + " pode me ajudar?")}><FaWhatsapp color="green"
+                                                                                                    size="22px"/></a> {item.Telefone}
+                            </Card.Footer>
+                        </Card.Body>
+                    </Card>
+                ))}
+            </CardColumns>
+
+        </>
+    );
 }
 
 export default App;
