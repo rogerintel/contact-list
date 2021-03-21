0d9d3331def6874877970be59ae0e05168ed07a6
Rogério Carvalho <rogerio.carvalho@unifesspa.edu.br>
2021-03-21 00:09:40 -0300

    Add checkBox

--- src/App.js
+++ src/App.js
@@ -9,12 +9,30 @@ function App() {
     const [data, setData] = useState([]);
     const [filtered, setFiltered] = useState([]);
     const inputRef = useRef(null);
+    const checkComRef = useRef(null);
+    const checkOpeRef = useRef(null);
 
     const onInputChanged = () => {
         setFiltered(data.filter(item => (
             item.Base.toLowerCase().includes(inputRef.current.value.toLowerCase()) ||
             item.Executivo.toLowerCase().includes(inputRef.current.value.toLowerCase())
         )));
+    }
+    const onCheckChanged = () => {
+        console.log(`comercial = ${checkComRef.current.checked} \n operacional = ${checkOpeRef.current.checked}`)
+        setFiltered(data.filter(item => (
+            item.Base.toLowerCase().includes(inputRef.current.value.toLowerCase()) ||
+            item.Executivo.toLowerCase().includes(inputRef.current.value.toLowerCase())
+        )));
+        if(checkOpeRef.current.checked){
+            if (!checkComRef.current.checked) {
+                setFiltered(filtered.filter(item => (item.Setor.toLowerCase().includes("operação"))));
+            }
+        } else {
+            if (checkComRef.current.checked) {
+                setFiltered(filtered.filter(item => (item.Setor.toLowerCase().includes("comercial"))));
+            }
+        }
     }
 
 
@@ -33,16 +51,30 @@ function App() {
     function maskFone(Telefone) {
         const regex = /^\(?([0-9]{2})\)?([0-9]{4,5})-?([0-9]{4})$/mg;
         const subst = `($1) $2-$3`;
-        const Tel2 = Telefone.replace(regex, subst);
-        return Tel2;
+        return Telefone.replace(regex, subst);
 
     }
-
     return (
         <Form style={{backgroundColor: 'rgba(229, 228, 235, 1)'}}>
-            <Form.Control  className="border border-primary rounded-pill mb-sm-5" ref={inputRef} type="text"
+            <Form.Control className="border border-primary rounded-pill" ref={inputRef} type="text"
                           placeholder="Pesquisar Base ou Executivo"
                           onChange={onInputChanged}/>
+            <Form.Group className="container">
+                <Form.Check ref={checkComRef}
+                    inline
+                    defaultChecked={false}
+                    label={"Comercial"}
+                    id="check-cargo-comercial"
+                    onClick={onCheckChanged}
+                />
+                <Form.Check ref={checkOpeRef}
+                    inline
+                    defaultChecked={false}
+                    label={"Operação"}
+                    id="check-cargo-operacional"
+                    onClick={onCheckChanged}
+                />
+            </Form.Group>
             <CardColumns className="m-2">
                 {filtered.map((item, i) => (
 
@@ -59,7 +91,7 @@ function App() {
                         <Card.Footer className="d-flex justify-content-around">
                             <a href={"https://api.whatsapp.com/send?phone=55" + item.Telefone + "&text="
                             + encodeURI("Olá, " + item.Executivo + " pode me ajudar?")} target="_blank" rel="noreferrer"
-                               >
+                            >
                                 <FaWhatsapp className="text-info" size="30px"/></a>
 
                             <a href={"tel:" + item.Telefone}
